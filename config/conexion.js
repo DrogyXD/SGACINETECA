6a635e769a40cc163901902520c0e9622e546a7f
Gabriel F <drogychanel@gmail.com>
2024-09-05 00:14:50 -0600

    cambios masivos

--- config/conexion.js
+++ config/conexion.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const conectarDB = async () => {
     try {
-        const dbUri = 'mongodb+srv://2123200401:Imposible98@clusterdeprueba.wdfctea.mongodb.net/SGACINETECA?retryWrites=true&w=majority&appName=Clusterdeprueba';
-        await mongoose.connect(dbUri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
+        await mongoose.connect(process.env.DB_MONGO, {
+
         });
         console.log('Base de datos conectada');
     } catch (error) {
-        console.error(error);
+        console.log(error);
         process.exit(1);
     }
 };
