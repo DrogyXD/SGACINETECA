055a1e1b6321060adeac47e67c8af5d288f5a169
Gabriel F <drogychanel@gmail.com>
2024-09-05 08:05:14 -0600

    cambios masivos 4

--- config/conexion.js
+++ config/conexion.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const conectarDB = async () => {
     try {
         await mongoose.connect(process.env.DB_MONGO, {
-
         });
         console.log('Base de datos conectada');
     } catch (error) {
